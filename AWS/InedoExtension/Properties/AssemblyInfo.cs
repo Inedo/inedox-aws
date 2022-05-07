88c2ca9991df569dc1ee6f14870926cd1b4ad366
Greg Divis <gdivis@inedo.com>
2022-05-06 21:21:19 -0400

    update to release inedo.sdk

--- AWS/InedoExtension/Properties/AssemblyInfo.cs
+++ AWS/InedoExtension/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using Inedo.Extensibility;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2021")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
